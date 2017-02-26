1b2598881169ffe0fdc314df95705316ae41b048
Lars-Kristian Johansen <larskristiankj@gmail.com>
2017-02-26 21:49:56 +0100

    New version

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -142,7 +142,7 @@
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
 
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 16);
+/******/ 	return __webpack_require__(__webpack_require__.s = 17);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -43447,7 +43447,7 @@ function CanvasRenderer() {
 
 /***/ }),
 
-/***/ 16:
+/***/ 17:
 /***/ (function(module, exports, __webpack_require__) {
 
 module.exports = __webpack_require__(0);
